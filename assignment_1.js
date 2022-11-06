4d26dadb5f5c45dda70a3cb741432775fcdb2a85
yayym001 <muhammad.tayyed@vcontractor.co.za>
2022-11-06 11:19:30 +0500

    first commit

--- assignment_1.js
+++ assignment_1.js
@@ -10,51 +10,61 @@ let arr1 = [11, 15, 17, 18, 20];
 arr1.sort();
 let num = arr1.min();
 let maxnumber = arr1.max();
+let newarray = [];
 for (num; num < maxnumber; num++) {
   if (arr1.includes(num) == false) {
-    console.log("missing number is : " + num);
+    newarray.push(num);
+    // console.log("missing number is : " + num+" and index of array is : ");
+  } else {
+    newarray.push(num);
   }
 }
-
-let array = ["testa", "testb"];
-Array.prototype.upperCase = function () {
-  var i;
-  for (i = 0; i < this.length; i++) {
-    this[i] = this[i].toUpperCase();
-  }
-};
-// Prototype of array function 1
-Array.prototype.lowercase = function () {
-  var i;
-  for (i = 0; i < this.length; i++) {
-    this[i] = this[i].toLowerCase();
-  }
-};
-
-// assignment no # 2
-
-// Prototype of array function 2
-array.upperCase();
-array.forEach((element) => {
-  console.log("array index : " + element);
+// console.log("missing number is : " + newarray);
+newarray.forEach(element => {
+  console.log("element : "+element)
+  if (arr1.includes(element) == false) {
+    console.log("missing number is : " + element+" and index of array is : "+newarray.indexOf(element));
+  } 
 });
-let array1 = ["Testa", "tEstb"];
-array.lowercase();
-array.forEach((element) => {
-  console.log("array index : " + element);
-});
+// let array = ["testa", "testb"];
+// Array.prototype.upperCase = function () {
+//   var i;
+//   for (i = 0; i < this.length; i++) {
+//     this[i] = this[i].toUpperCase();
+//   }
+// };
+// // Prototype of array function 1
+// Array.prototype.lowercase = function () {
+//   var i;
+//   for (i = 0; i < this.length; i++) {
+//     this[i] = this[i].toLowerCase();
+//   }
+// };
 
-// assignment no # 4
-// on browser side assignment_4.html created
+// // assignment no # 2
 
-function prompt() {
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  readline.question("Enter a nmber ", (number) => {
-    console.log(`hello, hi there ${number}`);
-    readline.close();
-  });
-}
-prompt();
+// // Prototype of array function 2
+// array.upperCase();
+// array.forEach((element) => {
+//   console.log("array index : " + element);
+// });
+// let array1 = ["Testa", "tEstb"];
+// array.lowercase();
+// array.forEach((element) => {
+//   console.log("array index : " + element);
+// });
+
+// // assignment no # 4
+// // on browser side assignment_4.html created
+
+// function prompt() {
+//   const readline = require("readline").createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+//   readline.question("Enter a nmber ", (number) => {
+//     console.log(`hello, hi there ${number}`);
+//     readline.close();
+//   });
+// }
+// prompt();
